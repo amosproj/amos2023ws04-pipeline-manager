6fe35873db6c454dd0550b3260aba498bdb533c9
CAgcoder <tginfo001@gmail.com>
2023-11-03 00:46:57 +0100

    First version of welcome page and header with some change of sign up page and login page

--- src/frontend/frontend/src/app/app-routing.module.ts
+++ src/frontend/frontend/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { LandingComponent } from './components/landing/landing.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  {path:'signup',component:SignupComponent}
+  { path: 'signup', component: SignupComponent },
+  {path:'',component:LandingComponent}
 ];
 
 @NgModule({
--- src/frontend/frontend/src/app/app.module.ts
+++ src/frontend/frontend/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { HeaderComponent } from './components/header/header.component';
+import { LandingComponent } from './components/landing/landing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     SignupComponent,
-    HeaderComponent
+    HeaderComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
