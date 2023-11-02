eef62fc4b8f2a85b0a0088b49fed8587c7b62c41
CAgcoder <tginfo001@gmail.com>
2023-11-02 01:05:42 +0100

    sign up page and login page first version still working on a welcome page

--- src/frontend/frontend/src/app/app.module.ts
+++ src/frontend/frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
