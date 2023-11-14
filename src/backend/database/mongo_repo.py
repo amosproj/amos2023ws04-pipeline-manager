854eab6903e1e2e1e734ca2ab13bb916cd0dcaa9
Artur Wasinger <art.wasinger@gmail.com>
2023-11-14 22:05:10 +0100

    feat(database): changed naming for databases
    Signed-off-by: Artur Wasinger <art.wasinger@gmail.com>

--- src/backend/database/mongo_repo.py
+++ src/backend/database/mongo_repo.py
@@ -1,7 +1,10 @@
 from pymongo import MongoClient
+import os
 
 client = MongoClient('localhost', 27017)
+# client = MongoClient(os.getenv(MONGODB_URL), os.getenv(MONGODB_PORT))
+
 db = client.flask_db
 user = db.user
-projects = db.projects
-datapipelines = db.datapipelines
+fileWPDB = db.fileWP
+datapipelineDB = db.datapipeline
