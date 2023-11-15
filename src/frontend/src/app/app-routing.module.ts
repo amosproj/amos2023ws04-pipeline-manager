a8fbde692976db38ac0983040c7a1430fa1d10e1
krutarth4 <krutarthparwal@gmail.com>
2023-11-15 20:45:03 +0100

    [Fix] [stable] refactored frontend code
    Signed-off-by: krutarth4 <krutarthparwal@gmail.com>
    Co-Authored-By: Tinggao Cui <113919834+CAgcoder@users.noreply.github.com>

--- src/frontend/src/app/app-routing.module.ts
+++ src/frontend/src/app/app-routing.module.ts
@@ -1,29 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
-import { SignupComponent } from './components/signup/signup.component';
 import { LandingComponent } from './components/landing/landing.component';
 import { UserComponent } from './components/user/user.component';
-import { UploadFileComponent } from './components/uploadFile/upload-file/upload-file.component';
-// import { ProjectComponent } from './components/project/project.component';
-// import { PiplineComponent } from './components/pipline/pipline.component';
-// import { DownloadComponent } from './components/download/download.component';
-
+import { UploadFileComponent } from './components/upload-file/upload-file.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'signup', component: SignupComponent },
   { path: '', component: LandingComponent },
   { path: 'user', component: UserComponent },
   { path: 'upload', component: UploadFileComponent },
-  // { path: 'project', component: ProjectComponent },
-  // { path: 'pipline', component: PiplineComponent },
-  // { path: 'download', component: DownloadComponent },
-
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/frontend/src/app/app.module.ts
+++ src/frontend/src/app/app.module.ts
@@ -2,43 +2,38 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
-import { SignupComponent } from './components/signup/signup.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LandingComponent } from './components/landing/landing.component';
 import { UserComponent } from './components/user/user.component';
-import { UploadFileComponent } from './components/uploadFile/upload-file/upload-file.component';
+import { UploadFileComponent } from './components/upload-file/upload-file.component';
 import { AlertModule } from '@full-fledged/alerts';
-// import { SideBarComponent } from './components/side-bar/side-bar.component';
-// import { ProjectComponent } from './components/project/project.component';
-// import { PiplineComponent } from './components/pipline/pipline.component';
-// import { DownloadComponent } from './components/download/download.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SignupComponent,
     HeaderComponent,
     LandingComponent,
     UserComponent,
+    SideBarComponent,
     UploadFileComponent,
-    // SideBarComponent,
-    // ProjectComponent,
-    // PiplineComponent,
-    // DownloadComponent
+    
+
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     AlertModule.forRoot({ maxMessages: 5, timeout: 5000 }),
-    HttpClientModule
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
