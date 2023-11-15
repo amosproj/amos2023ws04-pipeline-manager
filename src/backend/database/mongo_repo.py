d92cc508dcf881b25c252ce670793adfa8a9d947
krutarth4 <krutarthparwal@gmail.com>
2023-11-15 09:16:26 +0100

    DB initialization stopped
    Signed-off-by: krutarth4 <krutarthparwal@gmail.com>

--- src/backend/database/mongo_repo.py
+++ src/backend/database/mongo_repo.py
@@ -1,7 +1,9 @@
 from pymongo import MongoClient
 import os
 
-client = MongoClient('localhost', 27017)
+client = MongoClient('localhost', 27017,username='admin', 
+                         password='pass',
+                        authSource="admin")
 # client = MongoClient(os.getenv(MONGODB_URL), os.getenv(MONGODB_PORT))
 
 db = client.flask_db
