04a8d7a759668634ee46fe366d7f7035b764fb1f
krutarth4 <krutarthparwal@gmail.com>
2023-11-16 12:15:00 +0100

    testing docker for mongoDB and flask added
    Signed-off-by: krutarth4 <krutarthparwal@gmail.com>

--- src/backend/database/mongo_repo.py
+++ src/backend/database/mongo_repo.py
@@ -1,9 +1,7 @@
 from pymongo import MongoClient
 import os
 
-client = MongoClient('localhost', 27017,username='admin', 
-                         password='pass',
-                        authSource="admin")
+client = MongoClient('localhost', 27017)
 # client = MongoClient(os.getenv(MONGODB_URL), os.getenv(MONGODB_PORT))
 
 db = client.flask_db
