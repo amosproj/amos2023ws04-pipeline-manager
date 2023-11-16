622127dc4f4c60ed7a38e2f47b6f5c40aab1e8df
Artur Wasinger <art.wasinger@gmail.com>
2023-11-16 20:13:39 +0100

    maint(models): removed unecessary object params, reindented
    Signed-off-by: Artur Wasinger <art.wasinger@gmail.com>

--- src/backend/models/fileWP.py
+++ src/backend/models/fileWP.py
@@ -2,31 +2,27 @@ import uuid
 from database.mongo_repo import fileWPDB
 from flask import jsonify
 
-
 # represents a file work process
 class FileWP:
+    fileIds = []
 
-        uuid = None
-        name = None
-        datapipelineId = None
-        fileIds = []
-        def __init__(self, name, datapipelineId):
-            self.uuid = str(uuid.uuid4())
-            self.name = name
-            self.datapipelineId = datapipelineId
+    def __init__(self, name, datapipelineId):
+        self.uuid = str(uuid.uuid4())
+        self.name = name
+        self.datapipelineId = datapipelineId
 
-        def addFile(self, fileId):
-            # add file to object
-            self.fileIds.append(fileId)
-            # add file to db
-            result = fileWPDB.update_one({'uuid': self.uuid}, {'$set': {'fileIds': jsonify(self.fileIds)}})
+    def addFile(self, fileId):
+        # add file to object
+        self.fileIds.append(fileId)
+        # add file to db
+        result = fileWPDB.update_one({'uuid': self.uuid}, {'$set': {'fileIds': jsonify(self.fileIds)}})
 
-            return result
+        return result
 
-        def to_json(self):
-            return {
-                'uuid': str(self.uuid),
-                'name': self.name,
-                'datapipelineId': self.datapipelineId,
-                'fileIds': self.fileIds,
-            }
+    def to_json(self):
+        return {
+            'uuid': str(self.uuid),
+            'name': self.name,
+            'datapipelineId': self.datapipelineId,
+            'fileIds': self.fileIds,
+        }
