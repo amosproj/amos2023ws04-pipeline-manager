529182888615345cf32b7224fa89d03e27406678
sravanthidatla78 <sravanthidatla78@gmail.com>
2023-11-17 19:52:08 +0100

    merging changes from main(rebasing)

--- src/backend/app.py
+++ src/backend/app.py
@@ -8,6 +8,7 @@ from api.datapipeline import datapipeline
 from api.fileWP import fileWP
 from api.air_flow_api import air_flow_api
 from flask_restx import Api, Resource
+from dotenv import load_dotenv
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
