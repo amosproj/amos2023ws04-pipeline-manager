5dd9db551d0237010f49a99d8fcd5f8915981d43
Artur Wasinger <art.wasinger@gmail.com>
2023-11-28 01:12:12 +0100

    feature(list-datapipeline): added table to datapipeline
    Co-authored-by: Tinggao Cui <tginfo001@gmail.com>
    Signed-off-by: Artur Wasinger <art.wasinger@gmail.com>

--- src/frontend/src/app/pages/datapipeline/pages/list-datapipeline/list-datapipeline.component.ts
+++ src/frontend/src/app/pages/datapipeline/pages/list-datapipeline/list-datapipeline.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+
+interface Datapipeline {
+  uuid: string;
+  name: string;
+  config: string;
+}
+
 
 @Component({
   selector: 'app-list-datapipeline',
   templateUrl: './list-datapipeline.component.html',
   styleUrls: ['./list-datapipeline.component.scss']
 })
-export class ListDatapipelineComponent {
+export class ListDatapipelineComponent implements OnInit{
 
+  public datapiplines = new Observable<Datapipeline[]>;
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.datapiplines = this.http.get('http://localhost:8000/datapipeline') as Observable<Datapipeline[]>;
+  }
 }
