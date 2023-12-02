4bab509163f11a7f78d32fe1e803fc5b86c2d093
CAgcoder <tginfo001@gmail.com>
2023-12-02 12:09:44 +0100

    feat(edit function): finished the edit-datapipeline function
    sgin-off-by:TinggaoCui<tginfo001@gmail.com>

--- src/frontend/src/app/pages/datapipeline/pages/edit-datapipeline/edit-datapipeline.component.ts
+++ src/frontend/src/app/pages/datapipeline/pages/edit-datapipeline/edit-datapipeline.component.ts
@@ -1,29 +1,49 @@
-import { Component } from '@angular/core';
+import { Component,OnInit} from '@angular/core';
 import {FormBuilder} from "@angular/forms";
-import {DatapipelineService} from "../../../../core/services/datapipeline/datapipeline.service";
+import { DatapipelineService } from "../../../../core/services/datapipeline/datapipeline.service";
+import { ActivatedRoute } from '@angular/router';
+import { v4 as uuidv4 } from 'uuid';
+import { Observable } from 'rxjs';
+import { Entity } from 'aws-sdk/clients/costexplorer';
 
 @Component({
   selector: 'app-edit-datapipeline',
   templateUrl: './edit-datapipeline.component.html',
   styleUrls: ['./edit-datapipeline.component.scss']
 })
-export class EditDatapipelineComponent {
-
-  datapipelineForm = this.formBuilder.group({
-    name: '',
-    config: ''
-  });
+export class EditDatapipelineComponent{
 
   constructor(
     private datapipelineService: DatapipelineService,
     private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
   ) {
-
   }
 
+  ngOnInit(): void {
+    this.getID();
+  }
+  
+  getID(): string {
+    const id : string = this.route.snapshot.paramMap.get('id') ??'null value';
+    console.log(id);
+    return id
+  }
+  datapipelineForm = this.formBuilder.group({
+    uuid:this.getID(),
+    name: '',
+    config: ''
+  });
+  
   onSubmit(): void {
-    console.log(this.datapipelineForm.getRawValue());
-    this.datapipelineService.create(this.datapipelineForm.getRawValue());
+     this.datapipelineService.getById(this.getID()).subscribe(
+      entity =>  {
+        },
+      (error) => {
+        console.error('Error fetching entity:', error);
+      }
+    );
+    this.datapipelineService.update(this.getID(),this.datapipelineForm.getRawValue()).subscribe();
   }
 
 }
