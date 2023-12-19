26d9bdf4dcc7e321855ef898d2772860db04e936
Artur Wasinger <art.wasinger@gmail.com>
2023-12-19 15:11:10 +0100

    feature(auth_service): implemented public api wrapper for input to the server
    Signed-off-by: Artur Wasinger <art.wasinger@gmail.com>

--- src/backend/services/auth_service.py
+++ src/backend/services/auth_service.py
@@ -1,7 +1,7 @@
 import os
 
 from functools import wraps
-from flask import request, redirect, url_for, session
+from flask import redirect, url_for, session
 from dotenv import load_dotenv
 
 
@@ -14,6 +14,18 @@ def secure(f):
                 return redirect(url_for('auth'))
         else:
             print('keycloak not enabled')
+
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+def public(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        load_dotenv()
+        if (os.getenv('DPMS_PASSWORD') != session.get('password')
+                or os.getenv('DPMS_USERNAME') != session.get('username')):
+            print('Name or password for authentication is wrong')
 
         return f(*args, **kwargs)
     return decorated_function
