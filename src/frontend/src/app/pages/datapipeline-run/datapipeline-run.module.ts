25442640b5efbe77813f8ca191e515b10a4fa242
Artur Wasinger <art.wasinger@gmail.com>
2023-12-20 13:58:58 +0100

    wip
    Signed-off-by: Artur Wasinger <art.wasinger@gmail.com>

---
+++ src/frontend/src/app/pages/datapipeline-run/datapipeline-run.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListDatapipelineRunComponent } from './pages/list-datapipeline-run/list-datapipeline-run.component';
+
+
+
+@NgModule({
+  declarations: [
+    ListDatapipelineRunComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class DatapipelineRunModule { }
