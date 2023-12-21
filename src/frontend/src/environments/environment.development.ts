c18ded1301b8d3ec4a42bb7e0144c221e1d9a45a
Artur Wasinger <art.wasinger@gmail.com>
2023-12-21 23:03:32 +0100

    remove(env): removed unnecessary env variables
    Co-authored-by: Tinggao Cui <tginfo001@gmail.com>
    Co-authored-by: bhanuPrakashMa <bhanuprakashmaruboyina@gmail.com>
    Signed-off-by: Artur Wasinger <art.wasinger@gmail.com>

--- src/frontend/src/environments/environment.development.ts
+++ src/frontend/src/environments/environment.development.ts
@@ -1,8 +1,4 @@
 export const environment = {
     production: false,
-    AWS_ACCESS_KEY: "",
-    AWS_SECRET_KEY: "",
     SERVER_URL: "http://localhost:8000",
-    BACKEND_URL: "http://192.168.1.41:5000/"
-
 };
