536fa4606c21a54bbdbe1d002136f8a3c5358361
Artur Wasinger <art.wasinger@gmail.com>
2024-02-07 00:43:38 +0100

    feature(s3-upload-files): added refresh on file upload and did some refactoring
    Co-authored-by: Tinggao Cui <tginfo001@gmail.com>
    Signed-off-by: Artur Wasinger <art.wasinger@gmail.com>

--- src/frontend/src/app/pages/s3-upload-files/s3-upload-files.component.ts
+++ src/frontend/src/app/pages/s3-upload-files/s3-upload-files.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
-import { AirflowService } from 'src/app/core/services/airflow/airflow.service';
-import { DatapipelineRunService } from 'src/app/core/services/datapipeline-run/datapipeline-run.service';
-import { FileService } from 'src/app/core/services/file/file.service';
-import { S3FileUploadService } from 'src/app/core/services/s3-file-upload.service';
+import {Component, EventEmitter, Output} from '@angular/core';
+import {Observable, Subject} from 'rxjs';
+import {AirflowService} from 'src/app/core/services/airflow/airflow.service';
+import {DatapipelineRunService} from 'src/app/core/services/datapipeline-run/datapipeline-run.service';
+import {FileService} from 'src/app/core/services/file/file.service';
+import {S3FileUploadService} from 'src/app/core/services/s3-file-upload.service';
 
 @Component({
   selector: 'app-s3-upload-files',
@@ -11,39 +11,39 @@ import { S3FileUploadService } from 'src/app/core/services/s3-file-upload.servic
   styleUrls: ['./s3-upload-files.component.scss']
 })
 export class S3UploadFilesComponent {
-    startPipelineWithFile: boolean = false;
-    public selectedFile!: File;
-    public successMessage!: string;
-    public errorMessage!: string;
-    selectedFileName: string = 'File Name';
-    public dags$: Observable<any>;
-    public files$: Observable<any>;
-    selectedDag: any;
-    get_s3_uuid: any;
+  startPipelineWithFile: boolean = false;
+  public selectedFile!: File;
+  public successMessage!: string;
+  public errorMessage!: string;
+  selectedFileName: string = 'File Name';
+  public dags$: Observable<any>;
+  public files$: Observable<any>;
+  selectedDag: any;
+  get_s3_uuid: any;
 
-    constructor(private fileUploadService: S3FileUploadService,
-      private dpRunService: DatapipelineRunService,
-              private airflowService: AirflowService,
-              private fileService: FileService) { }
+  @Output() refreshEvent = new EventEmitter<any>();
 
-    selectFile() {
-      const fileInput: HTMLInputElement | null = document.querySelector('input[type="file"]');
-
-      if (fileInput !== null) {
-        fileInput.click();
-
-        fileInput.addEventListener('change', () => {
-          const selectedFile: File | undefined = fileInput.files?.[0];
-
-          if (selectedFile !== undefined) {
-            this.selectedFile = selectedFile;
-            this.selectedFileName = selectedFile.name;
-          }
-        });
-      }
+  constructor(private fileUploadService: S3FileUploadService,
+              private dpRunService: DatapipelineRunService,
+              private airflowService: AirflowService) {
   }
 
+  selectFile() {
+    const fileInput: HTMLInputElement | null = document.querySelector('input[type="file"]');
 
+    if (fileInput !== null) {
+      fileInput.click();
+
+      fileInput.addEventListener('change', () => {
+        const selectedFile: File | undefined = fileInput.files?.[0];
+
+        if (selectedFile !== undefined) {
+          this.selectedFile = selectedFile;
+          this.selectedFileName = selectedFile.name;
+        }
+      });
+    }
+  }
 
 
   uploadFileWithUrl() {
@@ -54,13 +54,15 @@ export class S3UploadFilesComponent {
     const formData = this.fileUploadService.getFormDataFromFile(this.selectedFile);
 
     this.fileUploadService.getPresignedUrl(this.selectedFile.name).subscribe(
-      (response: { presignedUrl: string, fileName: string, s3_uuid: string}) => {
-        const { presignedUrl, fileName, s3_uuid } = response;
+      (response: { presignedUrl: string, fileName: string, s3_uuid: string }) => {
+        const {presignedUrl, fileName, s3_uuid} = response;
         const get_s3_uuid = s3_uuid;
 
         this.uploadToPresignedUrl(presignedUrl, formData, fileName, s3_uuid, this.selectedFile.type);
 
-        console.log(presignedUrl)
+        console.log(presignedUrl);
+
+        this.refreshEvent.emit(null);
       },
       (error) => {
         console.error('Error getting presigned URL:', error);
@@ -73,47 +75,40 @@ export class S3UploadFilesComponent {
       (response) => {
         this.successMessage = 'File uploaded successfully!';
         this.createFileDetails(fileName, s3_uuid, mime_type)
-        console.log(response);
-        if(this.startPipelineWithFile==true)
-        {
+        if (this.startPipelineWithFile == true) {
           this.startPipeline()
         }
       },
       (error) => {
-        console.error('Error uploading file:', error);
-      this.errorMessage = 'Error uploading file. Please try again.';
-
+        this.errorMessage = 'Error uploading file. Please try again.';
       }
     );
   }
+
   private createFileDetails(fileName: string, s3_uuid: string, mime_type: string): void {
     this.get_s3_uuid = s3_uuid
     this.fileUploadService.createFileDetails(fileName, s3_uuid, mime_type).subscribe(
-      (response) =>{
-        console.log(this.get_s3_uuid)
+      (response) => {
       },
       (error) => {
-        console.error('Error storing file:', error);
-      this.errorMessage = 'Error Storing file. Please try again.';
+        this.errorMessage = 'Error Storing file. Please try again.';
 
       })
   }
 
   ngOnInit(): void {
     this.dags$ = this.airflowService.getAllDags();
-    }
+  }
 
   startPipeline() {
     if (this.selectedFile && this.selectedDag) {
       this.dpRunService.create({"datapipelineId": this.selectedDag.dag_id, "fileId": this.get_s3_uuid})
         .subscribe((value: any) => {
           const executionId = value?.object.executionId;
-          console.log(executionId);
-          console.log(this.get_s3_uuid);
           // TODO dont subscribe in a subscribe q_q but for now it can work
           this.dpRunService.startDatapipelineRun(executionId).subscribe();
           this.successMessage = 'File uploaded and started pipeline successfully!';
-        } );
+        });
     } else {
       throw Error("File and/or dag not selected.");
     }
--- src/frontend/src/app/pages/s3bucketfiles/list-s3bucketfiles/list-s3bucketfiles.component.ts
+++ src/frontend/src/app/pages/s3bucketfiles/list-s3bucketfiles/list-s3bucketfiles.component.ts
@@ -1,7 +1,24 @@
-import {Component, OnInit, OnDestroy, ViewChild} from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnDestroy,
+  ViewChild,
+  ChangeDetectorRef,
+  AfterContentInit,
+  AfterViewInit
+} from '@angular/core';
 import {RestApiService} from 'src/app/core/services/restApi/rest-api.service';
 import {FileService} from "../../../core/services/file/file.service";
-import {Subject, Observable, Subscription} from "rxjs";
+import {
+  Subject,
+  Observable,
+  Subscription,
+  combineLatest,
+  forkJoin,
+  ReplaySubject,
+  switchMap,
+  BehaviorSubject, debounceTime
+} from "rxjs";
 import {s3PresignedUploadInfo} from "../../../entity/s3";
 import {MaterialMoudule} from 'src/material-moudule';
 import {MatPaginator} from '@angular/material/paginator';
@@ -19,18 +36,16 @@ import {FileList} from "../../../entity/fileList";
 })
 
 export class ListS3bucketfilesComponent implements OnInit, OnDestroy, MaterialMoudule {
-  private selectedFile: File | null = null;
-  uploadedFiles: string[] = [];
-  downloadheader: any;
-  public fileDownload = new MatTableDataSource<FileList>();
+  public fileDownload$ = new BehaviorSubject<MatTableDataSource<FileList>>(new MatTableDataSource<FileList>());
   public temp: any;
   public upload_url_info: s3PresignedUploadInfo | null = null;
+
+  refreshToken$: Subject<void> = new Subject<void>();
 
   nameFilter = new FormControl('');
   fileTypeFilter = new FormControl('');
   lastModifiedFilter = new FormControl('');
   sizeFilter = new FormControl('');
-  storageClassFilter = new FormControl('');
   s3UuidFilter = new FormControl('');
   userFilter = new FormControl('');
 
@@ -45,138 +60,116 @@ export class ListS3bucketfilesComponent implements OnInit, OnDestroy, MaterialMo
   }
 
 
-  dtTrigger: Subject<any> = new Subject<any>();
   public filesSubscription: Subscription;
   private downloadSubscription: Subscription;
+
+  private filterFunction = function (data: {
+    name: string;
+    mime_type: { toString: () => string; };
+    last_modified: string;
+    size: string;
+    s3_uuid: string;
+    user: string;
+  }, filter: string): boolean {
+    let searchTerms = JSON.parse(filter);
+    return data.name.toLowerCase().indexOf(searchTerms.name) !== -1
+      && data.mime_type.toString().toLowerCase().indexOf(searchTerms.mime_type) !== -1
+      && data.last_modified.toLowerCase().indexOf(searchTerms.last_modified) !== -1
+      && data.size.toLowerCase().indexOf(searchTerms.size) !== -1
+      && data.s3_uuid.toLowerCase().indexOf(searchTerms.s3_uuid) !== -1
+      && data.user.toLowerCase().indexOf(searchTerms.user) !== -1;
+  };
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
 
   constructor(private restapi: RestApiService, private fileService: FileService) {
-
+    this.filesSubscription = this.refreshToken$.pipe(
+      debounceTime(1000),
+      switchMap(() => this.fileService.getAll())
+    ).subscribe(
+      (res) => {
+        this.temp = res;
+        const matTable = new MatTableDataSource<FileList>(this.temp)
+        matTable.filterPredicate = this.filterFunction;
+        matTable.paginator = this.paginator;
+        matTable.sort = this.sort;
+        this.fileDownload$.next(matTable);
+      });
   }
 
   displayedColumns: string[] = ['name', 'mime_type', 'last_modified', 'size', 's3_uuid', 'user', 'action'];
 
   ngOnInit(): void {
-    this.getAll();
-    this.fileService.get_upload_url().subscribe((value) => this.upload_url_info = value);
+    this.refreshToken$.next();
 
+    this.fileService.get_upload_url().subscribe((value) => this.upload_url_info = value);
 
     this.nameFilter.valueChanges
       .subscribe(
         name => {
           this.filterValues.name = String(name);
-          this.fileDownload.filter = JSON.stringify(this.filterValues);
+          this.refreshingMaterialDataTable();
         }
       )
     this.fileTypeFilter.valueChanges
       .subscribe(
         mime_type => {
           this.filterValues.mime_type = String(mime_type);
-          this.fileDownload.filter = JSON.stringify(this.filterValues);
+          this.refreshingMaterialDataTable();
         }
       )
     this.lastModifiedFilter.valueChanges
       .subscribe(
         last_modified => {
           this.filterValues.last_modified = String(last_modified);
-          this.fileDownload.filter = JSON.stringify(this.filterValues);
+          this.refreshingMaterialDataTable();
         }
       )
     this.sizeFilter.valueChanges
       .subscribe(
         size => {
           this.filterValues.size = String(size);
-          this.fileDownload.filter = JSON.stringify(this.filterValues);
+          this.refreshingMaterialDataTable();
         }
       )
     this.s3UuidFilter.valueChanges
       .subscribe(
         s3_uuid => {
           this.filterValues.s3_uuid = String(s3_uuid);
-          this.fileDownload.filter = JSON.stringify(this.filterValues);
+          this.refreshingMaterialDataTable();
         }
       )
     this.userFilter.valueChanges
       .subscribe(
         user => {
           this.filterValues.user = String(user);
-          this.fileDownload.filter = JSON.stringify(this.filterValues);
+          this.refreshingMaterialDataTable();
         }
       )
 
 
   }
 
-  createFilter(): (data: any, filter: string) => boolean {
-    let filterFunction = function (data: {
-      name: string;
-      mime_type: { toString: () => string; };
-      last_modified: string;
-      size: string;
-      s3_uuid: string;
-      user: string;
-    }, filter: string): boolean {
-      let searchTerms = JSON.parse(filter);
-      return data.name.toLowerCase().indexOf(searchTerms.name) !== -1
-        && data.mime_type.toString().toLowerCase().indexOf(searchTerms.mime_type) !== -1
-        && data.last_modified.toLowerCase().indexOf(searchTerms.last_modified) !== -1
-        && data.size.toLowerCase().indexOf(searchTerms.size) !== -1
-        && data.s3_uuid.toLowerCase().indexOf(searchTerms.s3_uuid) !== -1
-        && data.user.toLowerCase().indexOf(searchTerms.user) !== -1;
-    }
-    return filterFunction;
+  private refreshingMaterialDataTable() {
+    const matTable = this.fileDownload$.getValue();
+    matTable.filterPredicate = this.filterFunction;
+    matTable.paginator = this.paginator;
+    matTable.sort = this.sort;
+    matTable.filter = JSON.stringify(this.filterValues);
+    this.fileDownload$.next(matTable);
   }
-
 
 // ---------------------------------------------------------------------
 
 
   ngOnDestroy(): void {
-    this.dtTrigger.unsubscribe();
     if (this.filesSubscription) {
       this.filesSubscription.unsubscribe();
     }
     if (this.downloadSubscription) {
       this.downloadSubscription.unsubscribe();
-    }
-  }
-
-  getAll() {
-    this.filesSubscription = this.fileService.getAll().subscribe(
-      (res: FileList[]) => {
-        this.temp = res;
-        this.fileDownload = new MatTableDataSource<FileList>(this.temp);
-        this.fileDownload.paginator = this.paginator;
-        this.fileDownload.sort = this.sort;
-        this.fileDownload.filterPredicate = this.createFilter();
-        this.dtTrigger.next(null);
-        console.log(this.fileDownload)
-      })
-  }
-
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.fileDownload.filter = filterValue.trim().toLowerCase();
-
-    if (this.fileDownload.paginator) {
-      this.fileDownload.paginator.firstPage();
-    }
-  }
-
-  onFileSelected(event: any): void {
-    this.selectedFile = event.target.files[0];
-  }
-
-  upload(): void {
-    if (this.selectedFile) {
-      this.restapi.uploadCsvFile(this.selectedFile)
-        .then(() => console.log('File uploaded successfully'))
-        .catch(error => console.error('Error uploading file', error));
-    } else {
-      console.error('No file selected');
     }
   }
 
@@ -200,17 +193,10 @@ export class ListS3bucketfilesComponent implements OnInit, OnDestroy, MaterialMo
     });
   }
 
-  // handelPipelineResult() {
-
-  // }
-
-
-  // TODO
-  // upload_file_to_url(file: any) {
-  //   if (this.upload_url_info) {
-  //     this.fileService.upload_file_to_url(this.upload_url_info, file);
-  //   }
-  // }
+  refresh($event: any) {
+    // ignore event but trigger refresh token
+    this.refreshToken$.next();
+  }
 }
 
 
